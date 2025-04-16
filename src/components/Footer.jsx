40f5a7ef20ecb1db096ba175a7926e70b73b3d72
Yohanes Reynaldi <aldi.goceng@yahoo.com>
2025-04-17 00:17:40 +0700

    setup gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-50'> Footer</footer>
+    <footer className='bg-gray-50'>
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
+            <div>
+                
+            </div>
+        </div>
+    </footer>
   )
 }
 
